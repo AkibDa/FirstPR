f84acad4a7b51e4e541044d8270c4175beb3773f
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2026-05-09 19:12:24 +0530

    now backend will listen to the frontend url only

--- backend/main.py
+++ backend/main.py
@@ -45,7 +45,7 @@ app.add_middleware(SlowAPIMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["firstpr-mentor.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
